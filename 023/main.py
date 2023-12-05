27e04f19a3359bb56478e5b430a87fe0c256f312
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2023-12-05 12:53:20 +0100

    Day #023

---
+++ 023/main.py
@@ -0,0 +1,37 @@
+import time
+from turtle import Screen
+from player import Player
+from car import Car
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car = Car()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car.create_car()
+    car.move_cars()
+
+    for c in car.all_cars:
+        if c.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
