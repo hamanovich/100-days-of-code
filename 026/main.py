77f6cf5de474f8b83dc10077b4b3832c8891a17b
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2023-12-06 23:19:19 +0100

    Day #030

--- 026/main.py
+++ 026/main.py
@@ -3,6 +3,17 @@ from pandas import read_csv
 data = read_csv("nato_phonetic.csv")
 phonetic_dict = {row.letter: row.code for (_, row) in data.iterrows()}
 
-word = input("Enter your name: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter your name: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters from the alphabet available")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
