81f84e07787b4230c4fe705dce53df316944ad20
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2023-12-10 14:11:52 +0100

    Day #034

--- 017/main.py
+++ 017/main.py
@@ -1,13 +1,15 @@
+from html import unescape
 from question_model import Question
 from quiz_brain import QuizBrain
 from quiz_data import QuizData
+
 
 quiz_data = QuizData()
 question_data = quiz_data.get_questions()
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"]
+    question_text = unescape(question["question"])
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
--- 017/quiz_data.py
+++ 017/quiz_data.py
@@ -2,23 +2,27 @@ import requests
 
 
 class QuizData:
-    def __init__(self, difficulty='easy', amount='10'):
-        self.difficulty = difficulty
-        self.amount = amount
+    def __init__(self, difficulty="easy", amount="10"):
+        self.params = {
+            amount: amount,
+            difficulty: difficulty,
+            type: "boolean"
+        }
 
     def select_difficulty(self):
-        self.difficulty = input(
+        self.params["difficulty"] = input(
             "Select Difficulty (easy/medium/hard): ").lower()
 
     def select_amount(self):
-        self.amount = int(input("Select Number of Questions (from 2 to 30): "))
+        self.params["amount"] = int(
+            input("Select Number of Questions (from 2 to 30): "))
 
     def get_questions(self):
         self.select_difficulty()
         self.select_amount()
 
         response = requests.get(
-            f"https://opentdb.com/api.php?amount={self.amount}&difficulty={self.difficulty}&type=boolean")
+            "https://opentdb.com/api.php", params=self.params)
         _, results = response.json().values()
 
         return results
